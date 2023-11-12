24dd140f9db39f1896f34bd5a5a10c16b99a7550
Marcello <hey@marcello.codes>
2023-11-11 20:11:21 -0500

    🎨 Logo updates

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import styles from "./footer.scss?inline";
-import ImgLogoWhite from "~/media/images/logo-white.svg?jsx";
+import ImgLogo from "~/media/images/logo.svg?jsx";
 
 export default component$(() => {
   useStylesScoped$(styles);
@@ -8,11 +8,11 @@ export default component$(() => {
   return (
     <footer>
       <div class="container flex flex-col gap-3 py-2 text-center">
-        <div class="flex items-center justify-center gap-1 text-gray-500">
+        <div class="flex items-center justify-center gap-1">
           We <span class="text-red-400">&hearts;</span> Open Source
         </div>
-        <div class="flex items-center justify-center gap-2 opacity-40">
-          <ImgLogoWhite style={{ width: "24px", height: "24px" }} />
+        <div class="flex items-center justify-center gap-2">
+          <ImgLogo style={{ width: "24px", height: "24px" }} />
           &copy; bytebury 2023
         </div>
       </div>
